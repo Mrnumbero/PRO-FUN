e19c9976ab4715dcc18fa6e0b888776465d7f830
Sitthi Nawamawat <88361010+Mrnumbero@users.noreply.github.com>
2021-09-01 13:31:19 +0700

    Update 59.2.cpp

--- 59.2.cpp
+++ 59.2.cpp
@@ -2,12 +2,10 @@
 #include <string.h>
 int main()
 {
-	char str[20];
+    char str[20];
     scanf("%s", str);
-    //int i=strlen(str); 
-	//while(i--)
-	for(int i=strlen(str);i>=0;i--)
-	putchar(str[i]);
+    for(int i=strlen(str);i>=0;i--)
+    putchar(str[i]);
 	
 return 0;
 }
